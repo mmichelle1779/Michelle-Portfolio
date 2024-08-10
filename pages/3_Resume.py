b5e701ff6ecea2c6632c8bd4365ad169ae2b5fda
mmichelle1779 <mmichelle1779@gmail.com>
2024-08-10 14:22:47 -0700

    a lot going on

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -1,7 +1,26 @@
+from pathlib import Path
+
 import streamlit as st
 from PIL import Image
 
-st.set_page_config(page_title="My Resume", page_icon=":love_letter:", layout="wide")
+# Path Settings
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+css_file = current dir / "styles" / "main.css"
+resume_files = current_dir / "assets" / "Michelle Mar Resume.pdf"
+profile_pic = current_dir / "assets" / "Michelle LinkedIn Headshot.jpg"
 
+# General Settings
+PAGE_TITLE = "Resume | Michelle Mar"
+PAGE_ICON = ":love_letter:"
+NAME = "Michelle Mar"
+DESCRIPTION = """
+Undergraduate Student at UCSD
+"""
+EMAIL = "mimar@ucsd.edu"
+PROJECTS = {
+    "Women In Business Podcast: You Heard Her"
+}
+
+st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 st.title("Hello there!")
