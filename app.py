8a1ace29774b17ab3d3a8fab67ad124a12568480
mmichelle1779 <mmichelle1779@gmail.com>
2024-08-10 11:54:02 -0700

    Update app.py

--- app.py
+++ app.py
@@ -35,13 +35,12 @@ with st.sidebar:
         default_index=0, #optional
         orientation="horizontal",
         styles={
-            "container": {"padding": "0!important", "background-color"},
+            "container": {"padding": "0!important", "background-color": "white"},
             "icon": {"color": "orange", "font-size": "25px"},
             "nav-link": {
                 "font-size": "25px",
                 "text-align": "left",
                 "margin": "0px"
-                "--hover-color": "#eee",
             },
             "nav-link-selected": {"background-color": "green"},
         }
