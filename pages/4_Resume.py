e2ab4e45644e48e97d238709fee0574f737b6d54
mmichelle1779 <mmichelle1779@gmail.com>
2024-08-10 15:04:34 -0700

    headshot update

--- pages/4_Resume.py
+++ pages/4_Resume.py
@@ -1,13 +1,12 @@
 from pathlib import Path
-
 import streamlit as st
 from PIL import Image
+import io
 
 # Path Settings
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Michelle Mar Resume.pdf"
-profile_pic = current_dir / "assets" / "Michelle LinkedIn Headshot.png"
 
 # General Settings
 PAGE_TITLE = "Resume | Michelle Mar"
@@ -23,17 +22,21 @@ PROJECTS = {
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
-# Load CSS, PDF & Profile Pic
+# Load CSS
 with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+# Load Resume
 with open(resume_file, "rb") as pdf_file:
     PDFbyte = pdf_file.read()
-profile_pic = Image.open(profile_pic)
+
+# URL for Headshot
+url = "https://raw.githubusercontent.com/mmichelle1779/Michelle-Portfolio/main/pages/assets/Michelle%20LinkedIn%20Headshot.png"
 
 # Hero Section
 col1, col2 = st.columns(2, gap="small")
 with col1:
-    st.image(profile_pic, width=230)
+    st.image(url, width=200)
 
 with col2:
     st.title(NAME)
@@ -44,4 +47,4 @@ with col2:
         file_name=resume_file.name,
         mime="application/octet-stream",
     )
-    st.write(":love_letter:", EMAIL)
+    st.write(":love_letter: ", EMAIL)
