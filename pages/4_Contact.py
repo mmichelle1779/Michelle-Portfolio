bb67da5ed0f2a96dc80d5f7f008b2b345d666b4d
mmichelle1779 <mmichelle1779@gmail.com>
2024-08-10 17:23:25 -0700

    Update 4_Contact.py

--- pages/4_Contact.py
+++ pages/4_Contact.py
@@ -31,7 +31,6 @@ with st.container():
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
-        <input type="hidden" name="_next" value="https://michelle-portfolio.streamlit.app/">
         <textarea name="message" placeholder="Your message" required></textarea>
         <button type="submit">Send</button>
     </form>
