f22d5c39513f7823260de98dc345195dbe75bc79
mmichelle1779 <mmichelle1779@gmail.com>
2024-08-30 23:32:07 -0700

    Update 3_Podcast Project.py

--- pages/3_Podcast Project.py
+++ pages/3_Podcast Project.py
@@ -32,7 +32,7 @@ with st.container():
             """
         )
         st.write("Review my in-depth [product proposal]"
-                 "(https://docs.google.com/document/d/1vDb1Sjt3bEbyKkZzqr4WmFqzDMEAxsDINTeFEkJDUeM/edit) or see more below!")
+                 "(https://docs.google.com/document/d/1vDb1Sjt3bEbyKkZzqr4WmFqzDMEAxsDINTeFEkJDUeM/edit) or see more below.")
         st.write("Want a sneak peek? Check out this [clip]"
                  "(https://drive.google.com/file/d/1QUWe0NPRsF8MLLswyb0AyUamGvhnsvYW/view?usp=sharing) of our first episode!")
         st.markdown("Spotify Podcast link coming soon!")
